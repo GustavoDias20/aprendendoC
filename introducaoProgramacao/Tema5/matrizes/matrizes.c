933f2faac53ee2ac091a38f0c07063d431e83907
GustavoDias20 <gu2005dias@gmail.com>
2026-01-12 15:32:05 -0300

    .

---
+++ introducaoProgramacao/Tema5/matrizes/matrizes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    printf("O elemento na posição [0][0] é %d\n", matriz[0][0]);
+}
