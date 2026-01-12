85705d577cbd581dc890ea42bb480a02cc12aef7
GustavoDias20 <gu2005dias@gmail.com>
2026-01-12 15:58:04 -0300

    .

---
+++ introducaoProgramacao/Tema5/pratica/novato.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int index;
+
+    char * nomesAlunos[3][3] = {
+        {"Aluno 0", "Pt: 30", "Mat: 90"},
+        {"Aluno 1", "Pt: 60", "Mat: 60"},
+        {"Aluno 2", "Pt: 90", "Mat: 30"}
+    };
+
+    printf("Digite o numero do aluno que queira ver as notas... \n");
+    printf("Para o Aluno 0, Digite 0. \n Para o Aluno 1, Digite 1. \n Para o Aluno 2, Digite 2. \n");
+    scanf("%d", & index);
+
+    printf("As notas do %s são: %s\t %s", nomesAlunos[index][0], nomesAlunos[index][1], nomesAlunos[index][2]);
+}
